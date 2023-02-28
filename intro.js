9aa358d5191a7c1ded533a3c343d9681a3e2835c
Lane Vance <lanevance92@gmail.com>
2023-02-28 16:06:12 -0700

    testing

---
+++ intro.js
@@ -0,0 +1,43 @@
+
+
+// console.log("Hello, World")
+
+const firstName = `Lane`
+
+// console.log(firstName)
+
+let age = 30
+
+const isCool = true;
+
+// console.log(firstName, age, isCool)
+
+// console.log("Hello, my name is " + firstName + " I am " + age + " years old.")
+
+// console.log(`Hello, my name is ${firstName} I am ${age} years old.`)
+
+
+let x = 4
+let y = 10
+let z = x + 8
+
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// console.log(sum)
+
+if (x === y){
+    console.log(`X and Y are the same value`)
+}
+if (x < y){
+    console.log('X is smaller than Y')
+}
+
+if (x === y){
+    console.log(`X and Y are the same value`)
+}else{
+    console.log(`X and Y are different values`)
+}
